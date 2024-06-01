7c1d1aca259787c107947d92c802d6cf2170a136
gomin0 <gomin0981113@naver.com>
2024-06-02 02:53:18 +0900

    [refactor/#42] 개선하고 싶은 카테고리 조회 시 카테고리 별 점수 비율 같이 조회

--- src/main/java/neordinary/dofarming/domain/category/repository/CategoryJpaRepository.java
+++ src/main/java/neordinary/dofarming/domain/category/repository/CategoryJpaRepository.java
@@ -1,10 +1,7 @@
 package neordinary.dofarming.domain.category.repository;
 
 import neordinary.dofarming.domain.category.Category;
-import neordinary.dofarming.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface CategoryJpaRepository extends JpaRepository<Category, Long> {
 }
