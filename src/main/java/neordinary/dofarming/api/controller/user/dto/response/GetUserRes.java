bb11ce1de3a105888a721096c464ac367c0ee8de
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-06-02 00:21:44 +0900

    [feat/#13] 마이페이지 조회 추가

--- src/main/java/neordinary/dofarming/api/controller/user/dto/response/GetUserRes.java
+++ src/main/java/neordinary/dofarming/api/controller/user/dto/response/GetUserRes.java
@@ -1,4 +1,22 @@
 package neordinary.dofarming.api.controller.user.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class GetUserRes {
+
+    private String nickname;
+    private String profileImageUrl;
+    private List<GetPointRes> pointList;
+    private Integer lastWeekMissionSuccessRate;
+    private Integer thisWeekMissionSuccessRate;
+
 }
