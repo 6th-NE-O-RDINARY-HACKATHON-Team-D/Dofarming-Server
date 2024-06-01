1c2fe28c4984485b1dfa9e64f8e7bbec339825a0
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-06-01 14:47:37 +0900

    Initial commit

---
+++ src/main/java/neordinary/dofarming/DofarmingApplication.java
@@ -0,0 +1,13 @@
+package neordinary.dofarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DofarmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DofarmingApplication.class, args);
+	}
+
+}
