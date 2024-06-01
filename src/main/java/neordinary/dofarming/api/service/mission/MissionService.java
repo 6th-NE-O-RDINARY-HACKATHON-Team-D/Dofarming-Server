0b47d2b0f1077f005ea58685ea2c1dfa026d6884
이승연 <dltmddus9082@naver.com>
2024-06-02 01:18:12 +0900

    [feat/#25] 미션 인증 사진 업로드

--- src/main/java/neordinary/dofarming/api/service/mission/MissionService.java
+++ src/main/java/neordinary/dofarming/api/service/mission/MissionService.java
@@ -1,8 +1,12 @@
 package neordinary.dofarming.api.service.mission;
 
+import neordinary.dofarming.api.controller.mission.dto.UploadMissionImageRes;
 import neordinary.dofarming.domain.mission.Mission;
 import neordinary.dofarming.domain.user.User;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface MissionService {
     Mission recommendMission(User user);
+
+    UploadMissionImageRes uploadMissionImage(User user, Long missionId, MultipartFile file);
 }
